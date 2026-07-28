112ed11b8dc291d1c6b14f648666c552a05cb827
nlgpshelnax <shadairatzlaff@gmail.com>
2026-07-29 04:48:29 +0700

    Демо: каталог реальных занятий по 17 городам

---
+++ assets/map-pin-DsfGV2E4.js
@@ -0,0 +1,1 @@
+import{D as e}from"./index-CQcqWZaJ.js";var t=e(`check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]),n=e(`map-pin`,[[`path`,{d:`M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0`,key:`1r0f0z`}],[`circle`,{cx:`12`,cy:`10`,r:`3`,key:`ilqhr7`}]]);export{t as n,n as t};
